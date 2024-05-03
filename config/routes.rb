c01aa75e191ef614aba4b25843453741a0230c55
Elias Glyptis <eliasglyptis@gmail.com>
2024-05-03 21:27:16 +0300

    added tailwind flowbite, added devise for authentication

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'pages#home'
+  get 'about', to: "pages#about"
+  get 'dashboard', to: "pages#dashboard"
+  get 'contact', to: "pages#contact"
+  get 'thank_you', to: "pages#thank_you"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
