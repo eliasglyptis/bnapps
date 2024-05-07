c074c5933f3ecd89b306a4d2b08d1aba16cc445b
Elias Glyptis <eliasglyptis@gmail.com>
2024-05-07 17:06:23 +0300

    finished with dashboard action and dashboard view to show upcoming zoom meetings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,14 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
+
   def home
     @meetings = Meeting.upcoming
   end
 
-  def about
-  end
-
-  def contact
-  end
-
   def dashboard
+    @my_upcoming_meetings = Meeting.upcoming
+                                  .joins(:bookings)
+                                  .where("bookings.user_id = ?", current_user.id)
   end
 
   def thank_you
