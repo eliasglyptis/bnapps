a2b63f33cadaff64949476d55153c3bb701c81f6
Elias Glyptis <eliasglyptis@gmail.com>
2024-05-08 09:16:25 +0300

    implemented receipt template for user to view a receipt of payment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:dashboard]
+  before_action :authenticate_user!, only: [:dashboard, :receipt]
 
   def home
     @meetings = Meeting.upcoming
@@ -11,6 +11,10 @@ class PagesController < ApplicationController
                                   .where("bookings.user_id = ?", current_user.id)
 
     @my_bookings = current_user.bookings
+  end
+
+  def receipt
+    @booking = Booking.find(params[:booking_id])
   end
 
   def thank_you
